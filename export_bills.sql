39eddbee9055b6ca4cd5016da3923c596b29f7bd
James Turk <dev@jamesturk.net>
2019-11-25 13:49:38 -0500

    fixes for fields

--- export_bills.sql
+++ export_bills.sql
@@ -1,5 +1,5 @@
 copy (
-  select *
+  select b.*
   from opencivicdata_bill b
   join opencivicdata_organization o on b.from_organization_id=o.id 
   join opencivicdata_jurisdiction j on o.jurisdiction_id=j.id
@@ -7,7 +7,7 @@ copy (
 ) to '/tmp/bills.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billabstract x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -16,7 +16,7 @@ copy (
 ) to '/tmp/bill_abstracts.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billtitle x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -26,7 +26,7 @@ copy (
 
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billaction x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -35,7 +35,7 @@ copy (
 ) to '/tmp/bill_actions.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billidentifier x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -44,7 +44,7 @@ copy (
 ) to '/tmp/bill_identifiers.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billsource x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -53,7 +53,7 @@ copy (
 ) to '/tmp/bill_sources.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billsponsorship x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -62,7 +62,7 @@ copy (
 ) to '/tmp/bill_sponsorships.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billdocument x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -71,7 +71,7 @@ copy (
 ) to '/tmp/bill_documents.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billversion x 
   join opencivicdata_bill b ON x.bill_id = b.id
   join opencivicdata_organization o on b.from_organization_id=o.id 
@@ -80,7 +80,7 @@ copy (
 ) to '/tmp/bill_versions.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billversionlink x 
   join opencivicdata_billversion bv ON x.version_id = bv.id
   join opencivicdata_bill b ON bv.bill_id = b.id
@@ -90,7 +90,7 @@ copy (
 ) to '/tmp/bill_version_links.csv' with CSV HEADER;
 
 copy (
-  select *
+  select x.*
   from opencivicdata_billdocumentlink x 
   join opencivicdata_billdocument bd ON x.document_id = bd.id
   join opencivicdata_bill b ON bd.bill_id = b.id
--- export_votes.sql
+++ export_votes.sql
@@ -1,5 +1,5 @@
 copy (
-  select *,
+  select ve.*,
   (SELECT value FROM opencivicdata_votecount WHERE vote_event_id=ve.id AND option='yes') AS yes_count,
   (SELECT value FROM opencivicdata_votecount WHERE vote_event_id=ve.id AND option='no') AS no_count,
   (SELECT SUM(value) FROM opencivicdata_votecount WHERE vote_event_id=ve.id AND option != 'yes' AND option != 'no') AS other_count
@@ -9,10 +9,10 @@ copy (
   where j.name='North Carolina'
 ) to '/tmp/votes.csv' with CSV HEADER;
 
-copy (SELECT * 
+copy (SELECT pv.* 
   from opencivicdata_personvote pv
   JOIN opencivicdata_voteevent ve ON pv.vote_event_id=ve.id
   join opencivicdata_organization o on ve.organization_id=o.id 
   join opencivicdata_jurisdiction j on o.jurisdiction_id=j.id
   where j.name='North Carolina'
-) to '/tmp/person_votes.csv' with CSV HEADER;
+) to '/tmp/vote_person.csv' with CSV HEADER;

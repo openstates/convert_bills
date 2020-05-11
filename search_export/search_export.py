0e1a320ed4d0fbdd924fa18f64d8110b14b847c1
James Turk <dev@jamesturk.net>
2020-05-11 14:52:09 -0400

    json output instead of csv

--- search_export/search_export.py
+++ search_export/search_export.py
@@ -1,4 +1,4 @@
-import csv
+import json
 from django.contrib.postgres.search import SearchQuery
 from utils import init_django
 
@@ -12,6 +12,17 @@ class WebSearchQuery(SearchQuery):
     }
 
 
+def vote_to_dict(v):
+    return {
+        "date": v.start_date,
+        "identifier": v.identifier,
+        "motion_text": v.motion_text,
+        "result": v.result,
+        "counts": [{"option": c.option, "value": c.value} for c in v.counts.all()],
+        "votes": [{"option": v.option, "voter_name": v.voter_name} for v in v.votes.all()],
+    }
+
+
 def search_term(term, filename):
     from opencivicdata.legislative.models import Bill
     bills = Bill.objects.all().order_by("legislative_session__jurisdiction")
@@ -21,35 +32,36 @@ def search_term(term, filename):
     bills = bills.select_related("legislative_session", "legislative_session__jurisdiction",
                                  "searchable")
     print(term, len(bills))
-    fieldnames = ["state", "session", "identifier", "uuid", "title", "sponsors", "text_url"]
     with open("output/" + filename + ".csv", "w") as f:
-        out = csv.DictWriter(f, fieldnames)
-        out.writeheader()
         for b in bills:
             try:
                 link_url = b.searchable.version_link.url
             except Exception:
                 link_url = ""
-            out.writerow({
-                "state": b.legislative_session.jurisdiction,
+            bill = {
+                "state": b.legislative_session.jurisdiction.name,
                 "session": b.legislative_session.name,
                 "identifier": b.identifier,
                 "uuid": b.id,
                 "title": b.title,
-                "sponsors": "; ".join(s.name for s in b.sponsorships.all()),
+                "sponsors": [s.name for s in b.sponsorships.all()],
+                "actions": [{"date": a.date, "description": a.description} for a in b.actions.all()],
+                "votes": [vote_to_dict(v) for v in b.votes.all()],
                 "text_url": link_url,
-            })
-            with open(b.id.replace("ocd-bill/", "output/"), "w") as tf:
-                tf.write(b.searchable.raw_text)
+                "text": b.searchable.raw_text,
+            }
+            with open(f"output/{b.id.replace('ocd-bill/', '')}.json", "w") as f:
+                json.dump(bill, f)
 
 
 def main():
     init_django()
-    query = """flood reduce OR flood risk OR flood program OR flood damage OR flood measures OR flood development OR flood mitigation OR flood fund OR flood communities OR storm reduce OR storm risk OR storm program OR storm damage OR storm measures OR storm development OR storm mitigation OR storm fund OR storm communities OR disaster reduce OR disaster risk OR disaster program OR disaster damage OR disaster measures OR disaster development OR disaster mitigation OR disaster fund OR disaster communities OR flooding reduce OR flooding risk OR flooding program OR flooding damage OR flooding measures OR flooding development OR flooding mitigation OR flooding fund OR flooding communities OR nature reduce OR nature risk OR nature program OR nature damage OR nature measures OR nature development OR nature mitigation OR nature fund OR nature communities OR natural reduce OR natural risk OR natural program OR natural damage OR natural measures OR natural development OR natural mitigation OR natural fund OR natural communities OR wetlands reduce OR wetlands risk OR wetlands program OR wetlands damage OR wetlands measures OR wetlands development OR wetlands mitigation OR wetlands fund OR wetlands communities OR storms reduce OR storms risk OR storms program OR storms damage OR storms measures OR storms development OR storms mitigation OR storms fund OR storms communities"""
-    # query = "flood reduce OR flood risk"
-    # search_term("flood reduce", "reduce")
-    # search_term("flood risk", "risk")
-    search_term(query, "disasters")
+    # query = "epidemic OR pandemic OR flu OR cold OR health OR influenza OR swine flu OR covid OR coronavirus"
+    # # query = "flood reduce OR flood risk"
+    # # search_term("flood reduce", "reduce")
+    # # search_term("flood risk", "risk")
+    for word in ["epidemic", "pandemic", "flu", "cold", "influenza", "swine flu", "covid", "coronavirus"]:
+        search_term(word, word)
 
 
 if __name__ == "__main__":

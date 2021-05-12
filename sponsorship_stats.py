368abc8572fcfa0921730abc63e9d29a13f2d863
James Turk <dev@jamesturk.net>
2021-05-12 11:42:25 -0400

    by session

--- sponsorship_stats.py
+++ sponsorship_stats.py
@@ -1,15 +1,35 @@
-from openstates.data.models import BillSponsorship
+from openstates.data.models import BillSponsorship, LegislativeSession
 from openstates.metadata import STATES_BY_NAME
 import csv
 
 out = csv.writer(open("sponsorships.csv", "w"))
 
+out.writerow(("state", "session", "total", "unmatched", "percent_matched", "distinct"))
+
 for state in STATES_BY_NAME.values():
-    qs = BillSponsorship.objects.filter(bill__legislative_session__jurisdiction_id=state.jurisdiction_id)
+    session = (
+        LegislativeSession.objects.filter(jurisdiction_id=state.jurisdiction_id).exclude(classification="special")
+        .order_by("start_date")
+        .last()
+    )
+    print(state.name, session)
+    qs = BillSponsorship.objects.filter(bill__legislative_session=session)
     total_sponsorships = qs.count()
     unmatched_sponsorships = qs.filter(person_id=None).count()
-    distinct = qs.distinct("name").count()
-    out.writerow((state.name, total_sponsorships, unmatched_sponsorships, (total_sponsorships-unmatched_sponsorships)/total_sponsorships*100, distinct))
-    print(state.name)
-
-out.close()
+    distinct = qs.filter(person_id=None).distinct("name").count()
+    if total_sponsorships:
+        percent = (
+            (total_sponsorships - unmatched_sponsorships) / total_sponsorships * 100
+        )
+    else:
+        percent = 100
+    out.writerow(
+        (
+            state.name,
+            session.name,
+            total_sponsorships,
+            unmatched_sponsorships,
+            percent,
+            distinct,
+        )
+    )
